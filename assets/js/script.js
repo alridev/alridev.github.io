2155585b2b46e83e4dc18851f12c7cd27ddcd1f1
Alri <101062813+alridev@users.noreply.github.com>
2024-10-12 14:09:03 +0300

    Update script.js
    Add my new codestats profile

--- assets/js/script.js
+++ assets/js/script.js
@@ -67,7 +67,7 @@ for (let i = 0; i < navigationLinks.length; i++) {
   });
 }
 
-const url = "https://codestats.net/api/users/zcxw"; // alri
+const url = "https://codestats.net/api/users/alri"; 
 
 fetch(url)
   .then((res) => res.json())
